76678d58df444632fa39df9c591985fbb518e55b
CaptainStinkRat <116904070+CaptainStinkRat@users.noreply.github.com>
2023-01-31 11:11:21 -0600

    Update dndMakerGuts.py

--- dndMakerGuts.py
+++ dndMakerGuts.py
@@ -1,7 +1,19 @@
 from dndMaker import Menu
+import json
+
+class character:
+    def __init__(self,name,species,classSelect):
+        self.name = name
+        self.species = species
+        self.classSelect = classSelect
+    def toJson(self):
+        return json.dumps(self,indent=4,default=lambda o: o.__dict__)
 
 if __name__=='__main__':
     app = Menu()
     app.mainloop()
-    print(app.classSelection.get())
-    
+    if app.creation == True:
+        newCharacter = character(app.searchTerm.get(),app.speciesSelection.get(),app.classSelection.get())
+        newCharCreate = newCharacter.toJson()
+        with open('character.json','w') as outfile:
+            outfile.write(newCharCreate)
